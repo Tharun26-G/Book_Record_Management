cbdb17502ddcf811e98206a1c6c3e8f59255fcba
Tharun G <tharun26.g@gmail.com>
2022-11-03 20:10:17 +0530

    completed project setup

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const express = require("express");
 const app = express();
 
 const PORT = 8081;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "server is up and in running",
+
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running at port ${PORT}`);
+});
